655020f47420fbc15704f9fd5f684ea8adc57897
cjb68950 <cjb68950@uga.edu>
2026-03-25 16:24:06 -0400

    adding assignment1 shell script files and gitignore

---
+++ Assignment1/UGAFootball.sh
@@ -0,0 +1,59 @@
+# Connor Boyle
+# INFO2000-SP26
+# 1/23/26
+mkdir dawgs
+cd dawgs
+touch KirbySmart.txt
+touch MikeBobo.txt
+touch AustinChambers.txt
+touch ConnorNorman.txt
+touch WilWells.txt
+touch ConnorFerguson.txt
+touch NasirJohnson.txt
+touch ThomasBlackshear.txt
+touch ColeSpeer.txt
+touch JeremyBell.txt
+touch TateNelms.txt
+touch MasonShort.txt
+touch JamalMeriweather.txt
+touch DanielCalhoun.txt
+touch DennisUzoChukwu.txt
+touch MalachiToliver.txt
+touch DuncanCarpenter.txt
+touch DentonShamburger.txt
+touch HenryWaller.txt
+touch CashJones.txt
+mkdir players
+mkdir coaches
+mv KirbySmart.txt coaches
+mv MikeBobo.txt coaches
+mv AustinChambers.txt coaches
+mv ConnorNorman.txt coaches
+mv WilWells.txt coaches
+mv ConnorFerguson.txt players
+mv NasirJohnson.txt players
+mv ThomasBlackshear.txt players
+mv ColeSpeer.txt players
+mv JeremyBell.txt players
+mv TateNelms.txt players
+mv MasonShort.txt players
+mv JamalMeriweather.txt players
+mv DanielCalhoun.txt players
+mv DennisUzoChukwu.txt players
+mv MalachiToliver.txt players
+mv DuncanCarpenter.txt players
+mv DentonShamburger.txt players
+mv HenryWaller.txt players
+mv CashJones.txt players
+mv coaches coachesAndStaff
+mkdir rivals
+touch rivals/alabama.txt
+touch rivals/tech.txt
+mkdir medals
+touch medals/2022.txt
+touch medals/2021.txt
+touch medals/1980.txt
+touch medals/1942.txt
+mkdir allFiles
+cp -R */*.txt allFiles
+rm -r rivals
