f074419cf40d45607db95f1ef05b73064e64d4c8
cjb68950 <cjb68950@uga.edu>
2026-03-25 16:26:54 -0400

    adding output files correlating to .bats previously uploaded

---
+++ Assignment1/disorganized_structure/organizer.sh
@@ -0,0 +1,16 @@
+# Connor Boyle
+# INFO2000-SP26
+# 1/23/26
+mv disorganized_structure organized_structure
+cd ./organized_structure
+mkdir entertainment
+mv entertainment*.txt entertainment
+mkdir recipes
+mv recipes*.txt recipes
+mkdir sports
+mv sports*.txt sports
+mkdir technology
+mv technology*.txt technology
+mkdir travel
+mv travel*.txt travel
+ls -R > output.txt
